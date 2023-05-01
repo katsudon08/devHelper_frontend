04add218fd2d343c34d0648dfe97da1b231653ed
matsu <matsuaki171101@gmail.com>
2023-05-01 12:41:45 +0900

    ローディングの実装

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box'
 
 const Loading = () => {
-  return (
-    <div>Loading</div>
-  )
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+        </Box>
+    )
 }
 
 export default Loading
