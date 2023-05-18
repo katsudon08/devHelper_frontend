d09d7992e66b7c1f9cee3a69a846c370b16203da
matsu <matsuaki171101@gmail.com>
2023-05-19 08:58:45 +0900

    UIの作り直しをしなければいけない

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { RouterConfig } from './Router/RouterConfig';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallBack from './components/error/ErrorFallBack';
 import Loading from './components/Loading';
+import SignIn from "./pages/auth/SignIn";
 
 function App() {
     return (
         // エラーバウンダリー
         <ErrorBoundary FallbackComponent={ErrorFallBack}>
-            {/* <RouterConfig /> */}
-            <Loading />
+            <RouterConfig />
+            {/* <Loading /> */}
+            {/* <SignIn /> */}
         </ErrorBoundary>
     )
 }
