e4db110bf458b015c8113ec50d391f184469eaec
matsu <matsuaki171101@gmail.com>
2023-05-19 10:42:49 +0900

    ルーティング

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
 import { RouterConfig } from './Router/RouterConfig';
-import { ErrorBoundary } from 'react-error-boundary';
-import ErrorFallBack from './components/error/ErrorFallBack';
-import Loading from './components/Loading';
-import SignIn from "./pages/auth/SignIn";
 
 function App() {
     return (
-        // エラーバウンダリー
-        <ErrorBoundary FallbackComponent={ErrorFallBack}>
-            <RouterConfig />
-            {/* <Loading /> */}
-            {/* <SignIn /> */}
-        </ErrorBoundary>
+        <RouterConfig />
     )
 }
 
